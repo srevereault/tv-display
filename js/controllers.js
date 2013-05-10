9bb2d92862a984c1a697fa932f1db7c3ef6955bf
ybonnel <ybonnel@gmail.com>
2013-05-10 13:22:13 +0200

    Add a view with all current talks of one day

--- js/controllers.js
+++ js/controllers.js
@@ -31,6 +31,59 @@ breizhcampRoom.controller('RoomsController',
     }
 );
 
+breizhcampRoom.controller('DayController',
+    function RoomController($scope, $routeParams, $filter, programService) {
+
+        $scope.loading = true;
+
+        $scope.getNextTalkIndex = function(track) {
+            var index = 0;
+            var time = new Date().getHours() * 100 + new Date().getMinutes();
+            angular.forEach(track.talks, function(talk) {
+                if (parseInt(talk.time.replace(':','')) < time) {
+                    index++;
+                }
+            });
+            return index;
+        };
+
+        $scope.getTalks = function() {
+
+            if ($scope.tracks) {
+                $scope.currentTalks = [];
+                $scope.nextTalks = [];
+
+                angular.forEach($scope.tracks, function(track) {
+                    console.log(track);
+                    var nextTalkIndex = $scope.getNextTalkIndex(track);
+                    $scope.currentTalks.push(track.talks[nextTalkIndex - 1]);
+                    programService.getTalk(track.talks[nextTalkIndex - 1], function(talkDetailled) {
+                        track.talks[nextTalkIndex - 1].detail = talkDetailled;
+                    });
+                    $scope.nextTalks.push(track.talks[nextTalkIndex]);
+                });
+            }
+        };
+
+        $scope.$on('timeChanged', function() {
+            $scope.getTalks();
+        });
+
+        programService.getProgram(function(program) {
+            $scope.program = program;
+
+            $scope.dayTalks = [];
+
+            $scope.tracks = program.jours[parseInt($routeParams.dayId)].tracks;
+
+            $scope.getTalks();
+
+            $scope.loading = false;
+        });
+
+    }
+);
+
 breizhcampRoom.controller('RoomController',
     function RoomController($scope, $routeParams, $filter, programService) {
 
